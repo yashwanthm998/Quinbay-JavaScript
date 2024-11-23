247e7a41a6b2fc897f6db8f7b68977c6111edea3
Yashwanth M <yashwanthm998@gmail.com>
2024-11-23 21:34:14 +0530

    ES6 Concept

---
+++ ES6/Create_Strings_using_Template_Literals.js
@@ -0,0 +1,12 @@
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"]
+  };
+  function makeList(arr) {
+    const failureItems = arr.map(item => `<li class="text-warning">${item}</li>`);
+  
+    return failureItems;
+  }
+  
+  const failuresList = makeList(result.failure);
---
+++ ES6/Set_Default_Parameters_for_Your_Functions.js
@@ -0,0 +1,2 @@
+
+const increment = (number, value=1) => number + value;
